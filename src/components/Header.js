cd03a4fbb3a1bf2f3c2087cb019cd39d87f78fcf
Neves888 <helen.vneves@gmail.com>
2022-05-25 10:16:59 -0300

    adiciona requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from '../pages/Loading';
 
@@ -29,6 +30,9 @@ class Header extends React.Component {
           { userName }
         </p>
         { logar && <Loading /> }
+        <Link exact data-testid="link-to-search" to="/search">Search</Link>
+        <Link exact data-testid="link-to-favorites" to="/favorites">Favorites</Link>
+        <Link exact data-testid="link-to-profile" to="/profile">Profile</Link>
       </header>
     );
   }
